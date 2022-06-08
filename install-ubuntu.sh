923f2cd083f8b6479a52a91cbba0f856865b77e1
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2022-06-08 16:02:36 +0530

    Add files via upload

---
+++ install-ubuntu.sh
@@ -0,0 +1,17 @@
+sudo snap install --classic code
+sudo apt install python3
+sudo apt install pip
+sudo apt install gimp
+sudo apt install gcc
+sudo apt install vlc
+
+pip install numpy
+pip install matplotlib
+pip install sympy
+pip install tensorflow
+pip install torch
+pip install scipy
+pip install pillow
+pip install pandas
+pip install selenium
+pip install scikit-learn
