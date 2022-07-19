9f5b588eff63186aafecdd84d5a52172fd81fccc
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2022-07-19 23:40:08 +0530

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,4 +1,7 @@
 sudo snap install --classic code
+sudo snap install julia
+
+
 sudo apt install python3
 sudo apt install pip
 sudo apt install gimp
@@ -15,3 +18,8 @@ pip install pillow
 pip install pandas
 pip install selenium
 pip install scikit-learn
+pip install jupyter
+
+julia -e 'using Pkg; Pkg.add("IJulia")'
+
+echo "alias jupy='(cd ~/Code/dev/Jupyter && jupyter notebook)'" >> ~/.bashrc
