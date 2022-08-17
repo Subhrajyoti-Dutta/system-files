99a4c864b871be06a4e10daae55bda3376ea3666
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2022-08-17 08:19:16 +0530

    Delete upgrade-fedora.sh

--- upgrade-fedora.sh
+++
@@ -1,8 +0,0 @@
-sudo dnf upgrade
-sudo yum update
-sudo dnf update
-sudo dnf upgrade
-sudo yum update
-sudo dnf update
-
-for i in $(pip list --outdated | awk '{ print $1 }'); do pip install $i --upgrade; done
