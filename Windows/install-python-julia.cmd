cfb31724cfc6af7e517787f2c25052e53300fd07
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2023-08-01 11:12:46 +0530

    Restructuring

---
+++ Windows/install-python-julia.cmd
@@ -0,0 +1,13 @@
+pip install numpy
+pip install matplotlib
+pip install sympy
+pip install tensorflow
+pip install torch
+pip install scipy
+pip install pillow
+pip install pandas
+pip install selenium
+pip install scikit-learn
+pip install jupyter
+
+julia -e 'using Pkg; Pkg.add("IJulia")'
