abbdef268af4fc6ae159bb169bf29e639babb37f
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2024-11-19 19:43:09 -0500

    Update update-fedora.sh

--- Fedora/update-fedora.sh
+++ Fedora/update-fedora.sh
@@ -1,8 +1,10 @@
 sudo dnf upgrade
-sudo yum update
+sudo yum upgrade
 sudo dnf update
+sudo yum update
 sudo dnf upgrade
-sudo yum update
+sudo yum upgrade
 sudo dnf update
+sudo yum update
 
 for i in $(pip list --user --outdated | awk '{ print $1 }'); do pip install $i --upgrade; done
