4a21e57e455fe3ca86a1cd889e40d675e59be8d6
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2025-01-30 23:26:35 +0530

    Update grub-install.sh

--- Other Linux Files/grub-install.sh
+++ Other Linux Files/grub-install.sh
@@ -1,5 +1,5 @@
 sudo update-grub
-sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=manjaro --recheck
+sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=fedora --recheck
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 lsblk -o
 lsblk -o PATH,PTTYPE,PARTTYPE,FSTYPE,PARTTYPENAME
